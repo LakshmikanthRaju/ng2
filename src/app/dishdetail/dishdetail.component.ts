b8887265b908852e7c07ac492f5d2db4609e0f96
Lakshmikanth Raju <lraju@vmware.com>
2017-09-17 23:13:29 +0530

    Assignment:3-Dish Comment form - validity not working

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import 'rxjs/add/operator/switchMap';
 
 import { DishService } from '../services/dish.service';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -22,28 +24,88 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
 
-  constructor(private dishService:DishService,
-    private route:ActivatedRoute,
-    private location:Location) { }
+  commentForm: FormGroup
+  newComment: Comment;
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
+
+  constructor(private dishService: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private fb: FormBuilder) {
+
+      this.createForm();
+  }
 
   ngOnInit() {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params:Params) => this.dishService.getDish(+params['id']))
+      .switchMap((params: Params) => this.dishService.getDish(+params['id']))
       .subscribe(dish => {
         this.dish = dish;
         this.setPrevNext(dish.id);
       });
-
   }
-  
+
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onValueChanged(data?: any){
+    if(!this.commentForm) {  return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
   setPrevNext(dishId: number) {
     let index = this.dishIds.indexOf(dishId);
-    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
-    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
-  goBack():void {
+  onSubmit(): void {
+    this.newComment = this.commentForm.value;
+    this.newComment.date = new Date().toDateString();
+    this.dish.comments.push(this.newComment);
+    
+    console.log(this.newComment);
+    this.commentForm.reset({
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      rating: 5,
+      comment: ['', Validators.required]
+    });
+  }
+
+  goBack(): void {
     this.location.back();
   }
 
