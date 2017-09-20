a79567ba4f89961223880681fc12837c17f20548
Lakshmikanth Raju <lraju@vmware.com>
2017-09-20 11:21:14 +0530

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,6 +4,8 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 import 'rxjs/add/operator/switchMap';
 
 import { DishService } from '../services/dish.service';
@@ -13,7 +15,18 @@ import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)', opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)', opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ]),
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -28,6 +41,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup
   newComment: Comment;
   errMsg: string;
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -56,12 +70,16 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishService.getDish(+params['id']))
+      .switchMap((params: Params) => {
+        this.visibility = 'hidden';
+        return this.dishService.getDish(+params['id']);
+      })
       .subscribe(
         dish => {
           this.dish = dish;
           this.dishcopy = dish;
           this.setPrevNext(dish.id);
+          this.visibility = 'shown';
         },
         errmsg => this.errMsg = errmsg);
   }
